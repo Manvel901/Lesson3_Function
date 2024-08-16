229c5c7817f4a7fc85694c020e437d01a127dddd
Manvel <Manvelnavoan@gmail.com>
2024-08-16 15:15:32 +0300

    add in Lessinns3/task1

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double DzSum(int a, int b, int c, int d)
+{
+    double er = a * b;
+    int at = c + d;
+    double result = er / at;
+    return result;
+}
+double result = DzSum(0, 5, 8, 9);
+Console.Write(result);
