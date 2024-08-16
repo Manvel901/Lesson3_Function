abc9ca8c41d614101739a9aab49cf54d8fb2e950
Manvel <Manvelnavoan@gmail.com>
2024-08-16 16:25:42 +0300

    add in lessons3/Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = 5;
+int[] arr = new int[n];
+int j = 0;
+while (j < n)
+{
+    Console.Write("Введите начальное (целое)число: ");
+    string input = Console.ReadLine();
+    arr[j] = Convert.ToInt32(input);
+    j++;
+}
+for (j = 0; j < n; j++)
+{
+    if (arr[j] % 3 == 0)
+    {
+        arr[j] = 0;
+    }
+
+}
+j = 0;
+while (j < n)
+{
+    Console.Write($"{arr[j]} ;");
+    j++;
+}
