60a7c6b7005c76a60881640f5ba34339305a4588
Manvel <Manvelnavoan@gmail.com>
2024-08-16 15:27:41 +0300

    add in Lessinns3/task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿void GetSquare(int limit)
+{
+    int j = 1;
+    while (j <= limit)
+    {
+        Console.Write($"{j * j} ");
+        j++;
+    }
+}
+GetSquare(5);
+GetSquare(10);
+GetSquare(15);
