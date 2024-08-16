9645bcf872af0c8c33456e7f11ffe69514705945
Manvel <Manvelnavoan@gmail.com>
2024-08-16 15:42:52 +0300

    add in lessons3/Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿void ZeroElement(int[] arr)
+{
+    for (int j =0; j<arr.Length; j++)
+    {
+        if (arr[j] % 2 == 0)
+        {
+            arr[j]=0;
+        }
+    }
+}
+void PrintElement(int[] arr)
+{
+    foreach( int e in arr)
+    {
+        Console.Write($"{e} ");
+
+    }
+}
+int[] array ={41,6,81,22,9,5,34};
+ZeroElement(array);
+PrintElement(array);
