0dcbdde568c2be9850ca6e9072127d3f384549a6
Manvel <Manvelnavoan@gmail.com>
2024-08-16 16:08:09 +0300

    add in lessons3/Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rnd = new Random();
+int n = 100;
+int[] arr = new int[n];
+int j = 0;
+while (j < n)
+{
+    arr[j] = rnd.Next(1, 3);
+    j++;
+}
+j=0;
+while (j<n)
+{
+    Console.Write($"{arr[j]} ");
+    j++;
+}
